d9dede6390b3aa1791ef11b60e703ff01218c3e2
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-09-30 12:05:19 +0900

    membuat video capture

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import cv2
+import mediapipe as mp
 
 
-print("OpenCV version:")
+
+
+# Membuka Kamera
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+    print("Kamera tidak terbuka")
+    exit()
+
+while(cap.isOpened()):
+    ret,frame = cap.read()
+    if not ret:
+        print("gagal menangkap frame")
+        break
+
+    cv2.imshow("Hands gesture",frame)
+
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
