91cea89818a02396981df645541335871520dbd3
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-11-01 22:31:09 +0900

    memodifikasi username dan password agar bisa di hover

--- Login/appLogin.py
+++ Login/appLogin.py
@@ -1,10 +1,5 @@
 from tkinter import *
 
-# membuat windownya
-root = Tk()
-root.title("Login Admin")
-root.geometry("300x300")
-
 user = "admin"
 passw = "admin"
 
@@ -14,18 +9,43 @@ def login():
     else:
         print("Login Gagal")
 
+
+# Membuat Entry dengan placeholder
+def add_placeholder(entry, text):
+    entry.insert(0, text)
+    entry.config(fg='grey')  # warna placeholder
+
+    def on_focus_in(event):
+        if entry.get() == text:
+            entry.delete(0, END)
+            entry.config(fg='white')
+
+    def on_focus_out(event):
+        if entry.get() == '':
+            entry.insert(0, text)
+            entry.config(fg='grey')
+
+    entry.bind('<FocusIn>', on_focus_in)
+    entry.bind('<FocusOut>', on_focus_out)
+
+# membuat windownya
+root = Tk()
+root.title("Login Admin")
+root.geometry("300x300")
+
 #membuat Label
 label = Label(root, text="Login Admin")
 label.pack()
 
-#membuat inputan
-userName = Entry(root, text="Username")
-# userName.insert(0,"User Name")
-userName.pack()
+# Entry Username
+userName = Entry(root)
+userName.pack(pady=10)
+add_placeholder(userName, "User Name")
 
-paswd = Entry(root, text="Password", show="*")
-# paswd.insert(0,"Password")
-paswd.pack()
+# Entry Password
+paswd = Entry(root, show="*")
+paswd.pack(pady=10)
+add_placeholder(paswd, "Password")
 
 # membuat tombol
 button = Button(root, text="Login", command = login)
